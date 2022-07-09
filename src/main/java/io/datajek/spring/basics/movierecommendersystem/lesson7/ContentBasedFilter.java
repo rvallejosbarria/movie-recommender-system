3a62e871a088bcacc03fb09696108db3e661586f
Roberto <rvallejosbarria@gmail.com>
2022-07-09 18:47:41 -0400

    Spring Basics - Constructor and Setter Injection

---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson7/ContentBasedFilter.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson7;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContentBasedFilter implements Filter {
+  // getRecommendations takes a movie as input and returns a list of similar movies
+  public String[] getRecommendations(String movie) {
+    // implement logic of content based filter
+    // return movie recommendations
+    return new String[] {"Happy Feet", "Ice Age", "Shark Tale"};
+  }
+}
