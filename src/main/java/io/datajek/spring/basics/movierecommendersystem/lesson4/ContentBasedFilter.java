497724f15326cf1a37a70dff5211b445c66f9619
Roberto <rvallejosbarria@gmail.com>
2022-07-09 18:27:22 -0400

    Spring Basics - Autowiring By Type - @Primary

---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson4/ContentBasedFilter.java
@@ -0,0 +1,15 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson4;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Primary
+public class ContentBasedFilter implements Filter {
+  // getRecommendations takes a movie as input and returns a list of similar movies
+  public String[] getRecommendations(String movie) {
+    // implement logic of content based filter
+    // return movie recommendations
+    return new String[] {"Happy Feet", "Ice Age", "Shark Tale"};
+  }
+}
---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson4/MovieRecommenderSystemApplication.java
@@ -0,0 +1,24 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson4;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class MovieRecommenderSystemApplication {
+  public static void main(String[] args) {
+    // ApplicationContext manages the beans and dependencies
+    ApplicationContext appContenxt = SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+
+    // use ApplicationContext to find which filter is being used
+		RecommenderImplementation recommender = appContenxt.getBean(RecommenderImplementation.class);
+
+    // call method to get recommendations
+		String[] result = recommender.recommendMovies("Finding Dory");
+
+    // display results
+		System.out.println(Arrays.toString(result));
+	}
+}
