561864f7d19dbfbbb87ad89ecc208cf587ce72be
Roberto <rvallejosbarria@gmail.com>
2022-07-14 08:21:45 -0400

    refactor: made a few comment corrections

--- src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson13/MovieRecommenderSystemApplication.java
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson13/MovieRecommenderSystemApplication.java
@@ -12,20 +12,25 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class MovieRecommenderSystemApplication {
   public static void main(String[] args) {
-    // ApplicationContext manages the beans and dependencies
-    // ApplicationContext appContext = SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+    //ApplicationContext manages the beans and dependencies
+    /*Change this:
+      ApplicationContext appContext =
+        SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+    */
+
+    //to this:
     AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext(MovieRecommenderSystemApplication.class);
 
-    // use ApplicationContext to find which filter is being used
+    //use ApplicationContext to find which filter is being used
 		RecommenderImplementation recommender = appContext.getBean(RecommenderImplementation.class);
 
-    // call method to get recommendations
+    //call method to get recommendations
 		String[] result = recommender.recommendMovies("Finding Dory");
 
-    // display results
+    //display results
 		System.out.println(Arrays.toString(result));
 
-    // close the application context
+    //close the application context
     appContext.close();
 	}
 }
