398fff02f3ae492b992d4aea6a0e1c725d16ed86
Roberto <rvallejosbarria@gmail.com>
2022-07-14 09:21:36 -0400

    XML Application Configuration

---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson14/ContentBasedFilter.java
@@ -0,0 +1,10 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson14;
+
+public class ContentBasedFilter implements Filter {
+  // getRecommendations takes a movie as input and returns a list of similar movies
+  public String[] getRecommendations(String movie) {
+    // implement logic of content based filter
+    // return movie recommendations
+    return new String[] {"Happy Feet", "Ice Age", "Shark Tale"};
+  }
+}
---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson14/MovieRecommenderSystemApplication.java
@@ -0,0 +1,30 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson14;
+
+import java.util.Arrays;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MovieRecommenderSystemApplication {
+  public static void main(String[] args) {
+    //load the configuration file
+    ClassPathXmlApplicationContext appContext =
+      new ClassPathXmlApplicationContext("appContext.xml");
+
+    //check the beans which have been loaded
+    System.out.println("\nBeans loaded:");
+    System.out.println(Arrays.toString(appContext.getBeanDefinitionNames()));
+
+    //retrieve bean from the application context
+    RecommenderImplementation recommender =
+      appContext.getBean("recommenderImplementation", RecommenderImplementation.class);
+
+    //call method to get recommendations
+		String[] result = recommender.recommendMovies("Finding Dory");
+
+    //display results
+    System.out.println();
+		System.out.println(Arrays.toString(result));
+
+    appContext.close();
+	}
+}
